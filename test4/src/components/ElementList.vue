042aa5736d15a583cb5c1c6791af382ecaace6e0
brenoafb <breno.afb@gmail.com>
2021-01-04 18:56:20 -0300

    Add initial fuzzy searching for #4

--- test4/src/components/ElementList.vue
+++ test4/src/components/ElementList.vue
@@ -2,7 +2,7 @@
   <div>
     <h2> List </h2>
     <input type="text" v-model="searchQuery" @change="updateSearch">
-    <div v-for="element in elements" v-bind:key="element.id">
+    <div v-for="element in filteredElements" v-bind:key="element.id">
       <Element v-bind:element="element"/>
       <!-- <p>{{element.id}} {{element.description}}</p> !-->
     </div>
@@ -12,6 +12,7 @@
 <script>
 
 import Element from './Element.vue'
+import FuzzySearch from 'fuzzy-search';
 
 export default {
   name: 'ElementList',
@@ -27,7 +28,15 @@ export default {
 
   data() {
     return {
-      searchQuery: ""
+      searchQuery: "",
+    }
+  },
+
+  computed: {
+    filteredElements: function() {
+      const searcher = new FuzzySearch(this.elements, Object.keys(this.elements[0]));
+      const results = searcher.search(this.searchQuery);
+      return results;
     }
   }
 }
